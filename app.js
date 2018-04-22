b716cd6123d859cdfc5947ccd5ecc48b2ca4fca8
Zeeshan Tamboli <zeeshan.tamboli@gmail.com>
2018-04-22 10:11:32 +0530

    Completed

---
+++ app.js
@@ -0,0 +1,94 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+const exphbs = require('express-handlebars');
+const nodemailer = require('nodemailer');
+const session = require('express-session');
+const flash = require('connect-flash');
+const path = require('path');
+
+const app = express();
+
+//Require file
+const auth = require('./config/auth');
+
+//Handlebars Middleware
+app.engine('handlebars', exphbs());
+app.set('view engine', 'handlebars');
+
+//Body parser Middleware
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+
+//Express-Session Middleware
+app.use(
+  session({
+    secret: 'secret',
+    resave: true,
+    saveUninitialized: true
+  })
+);
+
+//Use flash
+app.use(flash());
+
+//Global Variables
+app.use(function(req, res, next) {
+  res.locals.success_msg = req.flash('success_msg');
+  next();
+});
+
+//Static folder
+app.use(express.static(path.join(__dirname, 'public')));
+
+app.get('/', (req, res) => {
+  res.render('index');
+});
+
+app.post('/send', (req, res) => {
+  const output = `
+    <p>You have a new contact request</p>
+    <h3>Contact Details</h3>
+    <ul>
+      <li>Name: ${req.body.name}</li>
+      <li>Email: ${req.body.email}</li>
+      <li>Number: ${req.body.number}</li>
+    </ul>
+    <h3>Message</h3>
+    <p>${req.body.message}</p>
+  `;
+
+  let transporter = nodemailer.createTransport({
+    service: 'gmail',
+    host: 'smtp.gmail.com',
+    secure: false,
+    auth: {
+      user: auth.user,
+      pass: auth.password
+    },
+    tls: {
+      rejectUnauthorized: false
+    }
+  });
+
+  let mailOptions = {
+    from: `"Portfolio Website" <${auth.user}>`, // sender address
+    to: 'zeeshan.tamboli@gmail.com', // list of receivers
+    subject: 'Website Contact', // Subject line
+    html: output // html body
+  };
+
+  // send mail with defined transport object
+  transporter.sendMail(mailOptions, (error, info) => {
+    if (error) {
+      return console.log(error);
+    }
+    console.log('Message sent: %s', info.messageId);
+    console.log('Preview URL: %s', nodemailer.getTestMessageUrl(info));
+
+    req.flash('success_msg', 'Your details have been sent successfully!');
+    res.redirect('/');
+  });
+});
+
+const port = process.env.PORT || 3000;
+app.listen(port, () => console.log(`Server started on port ${port}`));
--- gulpfile.js
+++ gulpfile.js
@@ -1,43 +1,56 @@
-const gulp        = require('gulp');
+const gulp = require('gulp');
 const browserSync = require('browser-sync').create();
-const sass        = require('gulp-sass');
+const sass = require('gulp-sass');
 
 // Compile Sass & Inject Into Browser
 gulp.task('sass', function() {
-    return gulp.src(['node_modules/bootstrap/scss/bootstrap.scss', 'src/scss/*.scss'])
-        .pipe(sass())
-        .pipe(gulp.dest("src/css"))
-        .pipe(browserSync.stream());
+  return gulp
+    .src([
+      'node_modules/bootstrap/scss/bootstrap.scss',
+      'public/src/scss/*.scss'
+    ])
+    .pipe(sass())
+    .pipe(gulp.dest('public/src/css'))
+    .pipe(browserSync.stream());
 });
 
-// Move JS Files to src/js
+// Move JS Files to public/src/js
 gulp.task('js', function() {
-    return gulp.src(['node_modules/bootstrap/dist/js/bootstrap.min.js', 'node_modules/jquery/dist/jquery.min.js','node_modules/popper.js/dist/umd/popper.min.js'])
-        .pipe(gulp.dest("src/js"))
-        .pipe(browserSync.stream());
+  return gulp
+    .src([
+      'node_modules/bootstrap/dist/js/bootstrap.min.js',
+      'node_modules/jquery/dist/jquery.min.js',
+      'node_modules/popper.js/dist/umd/popper.min.js'
+    ])
+    .pipe(gulp.dest('public/src/js'))
+    .pipe(browserSync.stream());
 });
 
 // Watch Sass & Serve
 gulp.task('serve', ['sass'], function() {
+  browserSync.init({
+    server: './public/src'
+  });
 
-    browserSync.init({
-        server: "./src"  
-    });
-
-    gulp.watch(['node_modules/bootstrap/scss/bootstrap.scss', 'src/scss/*.scss'], ['sass']);
-    gulp.watch("src/*.html").on('change', browserSync.reload);
+  gulp.watch(
+    ['node_modules/bootstrap/scss/bootstrap.scss', 'public/src/scss/*.scss'],
+    ['sass']
+  );
+  gulp.watch('public/src/*.html').on('change', browserSync.reload);
 });
 
-// Move Fonts to src/fonts
+// Move Fonts to public/src/fonts
 gulp.task('fonts', function() {
-  return gulp.src('node_modules/font-awesome/fonts/*')
-    .pipe(gulp.dest('src/fonts'))
-})
+  return gulp
+    .src('node_modules/font-awesome/fonts/*')
+    .pipe(gulp.dest('public/src/fonts'));
+});
 
-// Move Font Awesome CSS to src/css
+// Move Font Awesome CSS to public/src/css
 gulp.task('fa', function() {
-  return gulp.src('node_modules/font-awesome/css/font-awesome.min.css')
-    .pipe(gulp.dest('src/css'))
-})
+  return gulp
+    .src('node_modules/font-awesome/css/font-awesome.min.css')
+    .pipe(gulp.dest('public/src/css'));
+});
 
-gulp.task('default', ['js','serve', 'fa', 'fonts']);
+gulp.task('default', ['js', 'serve', 'fa', 'fonts']);
